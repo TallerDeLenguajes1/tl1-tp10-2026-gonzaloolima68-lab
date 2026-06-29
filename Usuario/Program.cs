de6eaa491ebe850d8d64a555c99cda1c6c16cb3d
Gonzalo Olima <gonzaloolima68@gmail.com>
2026-06-29 01:44:16 -0300

    usuario para la api, url de la api y pedimos datos a la api

--- Usuario/Program.cs
+++ Usuario/Program.cs
@@ -7,5 +7,21 @@ using System.Threading.Tasks;
 
 namespace punto2
 {
-    
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            
+            //uruario para comunicarme con la api
+            HttpClient usuarioHttp=new HttpClient();
+            string apiurl="https://jsonplaceholder.typicode.com/users";
+            //pedimos los datos  a la api
+            HttpResponseMessage respuesta= await usuarioHttp.GetAsync(apiurl);
+
+            respuesta.EnsureSuccessStatusCode();
+
+
+
+        }
+    }
 }
